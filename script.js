cda2f5091b04630d52be9f3c1207f65d7467ea4a
Reback <gregreback@gmail.com>
2022-08-06 15:16:47 -0400

    add

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("dog").innerHTML = "I am a cat!";
+console.log("i am a cat");
+
+const hi = document.getElementById("dog");
+console.log(hi);
+hi.innerHTML = "Hiiii";
+console.log(hi);
